b1bbab2600f8b2b759ecaefa2911946aa46b6e78
mliu <mingxuanliu.miu@gmail.com>
2024-04-23 21:07:11 +0200

    init

--- download.sh
+++ download.sh
@@ -9,7 +9,7 @@ TARGET_FOLDER="."             # where all files should end up
 mkdir -p ${TARGET_FOLDER}
 
 if [[ $MODEL_SIZE == "" ]]; then
-    MODEL_SIZE="8B,8B-instruct,70B,70B-instruct"
+    MODEL_SIZE="70B,70B-instruct"
 fi
 
 echo "Downloading LICENSE and Acceptable Usage Policy"
